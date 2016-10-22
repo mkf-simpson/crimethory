8cd3a47d9644fde04d7cbae25de832f20ca9ef07
Konstantin Makarychev <mrsimpson@inbox.ru>
2016-10-22 22:28:24 +0300

    Add MQTT

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 "use strict";
 
 import express from 'express';
+import mqtt from 'mqtt';
 
 const app = express();
 app.use(express.static('public'));
 
-app.listen(3000, () => {
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+http.listen(3000, () => {
     console.log("Ready!");
+});
+
+io.on('connection', (socket) => {
+    var client  = mqtt.connect('mqtt://mosquitto:1888');
+
+    console.log(`New client: ${socket.id}`);
+    client.on('connect', function () {
+        console.log("connected to mqtt");
+        client.subscribe('crimethory');
+    });
+
+    client.on('message', function (topic, message) {
+        console.log(topic, message.toString());
+        if (topic === 'crimethory') {
+            socket.emit('mqtt message', message.toString());
+        }
+    });
 });
