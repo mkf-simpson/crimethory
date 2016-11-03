52ab59f8944c978280cd0eb6a887f71c0c8fcfdb
Konstantin Makarychev <mrsimpson@inbox.ru>
2016-11-03 16:57:05 +0300

    Refactor Twitter job

--- jobs/build.sbt
+++ jobs/build.sbt
@@ -1,16 +1,19 @@
 name := "crimethory"
 
-version := "0.0.17"
+version := "0.0.18"
 
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-    "io.hydrosphere" %% "mist" % "0.6.0",
+    "io.hydrosphere" %% "mist" % "0.6.2",
     "org.apache.spark" %% "spark-core" % "2.0.0",
     "org.apache.spark" %% "spark-sql" % "2.0.0",
     "org.apache.spark" %% "spark-hive" % "2.0.0",
     "org.apache.spark" %% "spark-mllib" % "2.0.0",
     "org.apache.spark" %% "spark-streaming" % "2.0.0",
-    "org.apache.spark" %% "spark-streaming-twitter" % "1.6.2"
-
+    "org.apache.bahir" %% "spark-streaming-twitter" % "2.0.0",
+    "org.twitter4j" % "twitter4j-stream" % "4.0.4",
+    "io.circe" %% "circe-core" % "0.5.1",
+    "io.circe" %% "circe-generic" % "0.5.1",
+    "io.circe" %% "circe-parser" % "0.5.1"
 )
--- jobs/src/main/scala/CrimeTwitter.scala
+++ jobs/src/main/scala/CrimeTwitter.scala
@@ -3,8 +3,16 @@ import io.hydrosphere.mist.lib.{MQTTPublisher, MistJob}
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
+
 
 object CrimeTwitter extends MistJob with MQTTPublisher {
+
+  case class Tweet(text: String, screenName: String, name: String, id: Long, imageURL: String)
+
   /** Contains implementation of spark job with ordinary [[org.apache.spark.SparkContext]]
     * Abstract method must be overridden
     *
@@ -12,17 +20,18 @@ object CrimeTwitter extends MistJob with MQTTPublisher {
     * @return result of the job
     */
   override def doStuff(parameters: Map[String, Any]): Map[String, Any] = {
-    println("crimethory start")
-    publish("crimethory start")
-
     context.setLogLevel("INFO")
 
     val ssc = new StreamingContext(context, Seconds(30))
     val stream = TwitterUtils.createStream(ssc, None, Array("#crimethory"))
     stream.foreachRDD { (rdd) =>
       val collected = rdd.collect()
-      collected.map({(x) => println(x.getText)})
-      collected.foreach({(x) => publish(s"[NEW TWEET][[${x.getText}]][[${x.getUser.getScreenName}]][[${x.getUser.getName}]][[${x.getId}]][[${x.getUser.getOriginalProfileImageURL}]]")})
+      val tweets = collected.map({(x) => Tweet(x.getText, x.getUser.getScreenName, x.getUser.getName, x.getId, x.getUser.getOriginalProfileImageURL)})
+      tweets.foreach { (x) =>
+        val str = x.asJson.noSpaces
+        println(str)
+        publish(str)
+      }
     }
 
     ssc.start()
