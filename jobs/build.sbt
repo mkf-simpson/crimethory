3457b44bf80c3fa8ccc2feddd388731646e5172e
Your Name <you@example.com>
2016-11-03 15:35:28 +0000

    Fix Twitter Mist job

--- jobs/build.sbt
+++ jobs/build.sbt
@@ -4,13 +4,6 @@ version := "0.0.18"
 
 scalaVersion := "2.11.8"
 
-mergeStrategy in assembly <<= (mergeStrategy in assembly) { (old) =>
-   {
-    case PathList("META-INF", xs @ _*) => MergeStrategy.discard
-    case x => MergeStrategy.first
-   }
-}
-
 libraryDependencies ++= Seq(
     "io.hydrosphere" %% "mist" % "0.6.2",
     "org.apache.spark" %% "spark-core" % "2.0.0",
@@ -19,8 +12,5 @@ libraryDependencies ++= Seq(
     "org.apache.spark" %% "spark-mllib" % "2.0.0",
     "org.apache.spark" %% "spark-streaming" % "2.0.0",
     "org.apache.bahir" %% "spark-streaming-twitter" % "2.0.0",
-    "org.twitter4j" % "twitter4j-stream" % "4.0.4",
-    "io.circe" %% "circe-core" % "0.5.1",
-    "io.circe" %% "circe-generic" % "0.5.1",
-    "io.circe" %% "circe-parser" % "0.5.1"
+    "org.twitter4j" % "twitter4j-stream" % "4.0.4"
 )
--- jobs/src/main/scala/CrimeTwitter.scala
+++ jobs/src/main/scala/CrimeTwitter.scala
@@ -3,11 +3,7 @@ import io.hydrosphere.mist.lib.{MQTTPublisher, MistJob}
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.twitter._
 
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-import io.circe.syntax._
-
+import twitter4j.Status
 
 object CrimeTwitter extends MistJob with MQTTPublisher {
   /** Contains implementation of spark job with ordinary [[org.apache.spark.SparkContext]]
@@ -22,14 +18,14 @@ object CrimeTwitter extends MistJob with MQTTPublisher {
     val ssc = new StreamingContext(context, Seconds(30))
     val stream = TwitterUtils.createStream(ssc, None, Array("#crimethory"))
     stream.foreachRDD { (rdd) =>
-      case class Tweet(text: String, screenName: String, name: String, id: Long, imageURL: String)
-
-      val collected = rdd.collect()
-      val tweets = collected.map({(x) => Tweet(x.getText, x.getUser.getScreenName, x.getUser.getName, x.getId, x.getUser.getOriginalProfileImageURL)})
-      tweets.foreach { (x) =>
-//        val str = x.asJson.noSpaces
-        println(x)
-//        publish(str)
+      val collected: Array[Status] = rdd.collect()
+      var idx = 0
+      while (idx < collected.length) {
+        val x = collected(idx)
+        val str = s"""{"text": "${x.getText}", "screenName": "${x.getUser.getScreenName}", "name": "${x.getUser.getName}", "id": "${x.getId}"}"""
+        publish(str)
+        println(str)
+        idx += 1
       }
     }
 
