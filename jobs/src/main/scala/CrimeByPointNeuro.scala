3768443ef315e2dc58f3776c986042ee12c8db3c
Leonid133 <lenin133@yandex.ru>
2016-11-08 20:10:13 +0300

    to old version

--- jobs/src/main/scala/CrimeByPointNeuro.scala
+++ jobs/src/main/scala/CrimeByPointNeuro.scala
@@ -1,33 +1,11 @@
-
 import io.hydrosphere.mist.lib.{MistJob, SQLSupport}
-
 import scala.math.BigInt
-import org.apache.spark.ml.classification.{MultilayerPerceptronClassificationModel, MultilayerPerceptronClassifier}
+
+import org.apache.spark.ml.classification.{MultilayerPerceptronClassifier, MultilayerPerceptronClassificationModel}
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.sql.SQLContext
-
-import java.nio.file.Paths
-import java.nio.file.Files
 
 object CrimeByPointNeuro extends MistJob with SQLSupport {
-
-  def predict(month: Int, x: Int, lat: Double, lon: Double, contextSQL: SQLContext): Double= {
-    if (Files.exists(Paths.get("model/data/_SUCCESS"))) {
-      val featureReq = Seq((1.0, Vectors.dense(month.toDouble / 12.0, x / 10.0, (math.abs(10.0*(lat+lon)).toDouble - math.abs(10.0*(lat+lon)).toInt))))
-      val requestData = contextSQL.createDataFrame(featureReq).toDF("label", "features")
-
-      val model = MultilayerPerceptronClassificationModel.load("model")
-      val prediction = model.transform(requestData)
-      val predictedCrime = prediction.select("prediction").head()
-      if (predictedCrime.size > 0)
-        println("crime: ", predictedCrime.toString)
-      val crimePoint = 0.5 * (predictedCrime(0).toString.toDouble / 30.0 + scala.math.abs(scala.math.cos((lat + lon + month) * x) + scala.math.cos((lat + lon + month) * x * 2) + scala.math.cos((lat + lon + month) * x * 3)))
-
-      crimePoint
-    }
-    else 0.0
-  }
 
   def doStuff(parameters: Map[String, Any]): Map[String, Any] = {
 
@@ -38,14 +16,62 @@ object CrimeByPointNeuro extends MistJob with SQLSupport {
     val lng = parameters("lng").asInstanceOf[String].toDouble
     val month = parameters("month").asInstanceOf[BigInt].intValue
 
+    var crimeCollection = collection.mutable.ArrayBuffer[(Double, org.apache.spark.ml.linalg.Vector)]()
+    for(x <- 1 to 5) {
+      for (monthT <- 1 to 12) {
+        crimeCollection += (((scala.math.abs(scala.math.cos(lat + lng + monthT))*100).toInt.toDouble, Vectors.dense(monthT.toDouble / 12.0)))
+      }
+    }
+
+    val data = contextSQL.createDataFrame(crimeCollection).toDF("label", "features")
+
+    data.show(15)
+
+    val splits = data.randomSplit(Array(0.9, 0.1), seed = 1234L)
+    val train = splits(0)
+    val test = splits(1)
+    val layers = Array[Int](1, 42, 100)
+
+    val trainer = new MultilayerPerceptronClassifier()
+      .setLayers(layers)
+      .setBlockSize(64)
+      .setSeed(1234L)
+      .setMaxIter(300)
+
+    val model = trainer.fit(train)
+
+    val result = model.transform(test)
+
+    result.show(15)
+
+    val predictionAndLabels = result.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setMetricName("accuracy")
+    println("accuracy " + evaluator.evaluate(predictionAndLabels))
+
+    val featureReq = Seq((1.0, Vectors.dense(month.toDouble/12.0)))
+
+    val requestData = contextSQL.createDataFrame(featureReq).toDF("label", "features")
+
+    requestData.show()
+
+    val prediction = model.transform(requestData)
+
+    val predictedCrime = prediction.select("prediction").head()
+    if(predictedCrime.size > 0 )
+      println("crime: ", predictedCrime.toString)
+
+    val crimePoint = predictedCrime(0).toString.toDouble
+
+
     val byPointK = Array(2, 5, 4, 3, 1)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * (crimePoint/100.0 + scala.math.abs(scala.math.sin((lat + lng + month) * x))));
 
     val byTypeK = Array(3, 4, 2, 1, 5)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * (crimePoint/100.0 + scala.math.abs(scala.math.sin((lat + lng + month) * x))));
 
     val historical = Array(4, 2, 3, 5, 1)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * (crimePoint/100.0 + scala.math.abs(scala.math.cos((lat + lng + month) * x) + scala.math.cos((lat + lng + month) * x * 2) + scala.math.cos((lat + lng + month) * x * 3))));
 
     Map(
       "by_point" -> byPointK.zipWithIndex.map({ case (s, i) => i.toString -> s }).toMap,
