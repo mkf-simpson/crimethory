40817145d2075a7cd619d4266a11567250865fe0
Konstantin Makarychev <mrsimpson@inbox.ru>
2016-11-08 16:13:12 +0300

    Update CrimeTwitter.scala

--- jobs/src/main/scala/CrimeTwitter.scala
+++ jobs/src/main/scala/CrimeTwitter.scala
@@ -12,21 +12,19 @@ object CrimeTwitter extends MistJob with MQTTPublisher {
     val ssc = new StreamingContext(context, Seconds(30))
     val stream = TwitterUtils.createStream(ssc, None, Array("#trump"))
     stream.foreachRDD { (rdd) =>
-      val collected: Array[Status] = rdd
-        .filter({ (tweet: Status) => {
-          !tweet.getText.startsWith("RT")
-        } })
-        .collect()
+      val collected: Array[Status] = rdd.collect()
       var idx = 0
       println(s"${collected.length} tweets found")
       while (idx < collected.length) {
         val x = collected(idx)
-        publish(Map(
-          "text" -> x.getText,
-          "screenName" -> x.getUser.getScreenName,
-          "name" -> x.getUser.getName,
-          "id" -> x.getId
-        ))
+        if (x.startsWith("RT")) {
+          publish(Map(
+            "text" -> x.getText,
+            "screenName" -> x.getUser.getScreenName,
+            "name" -> x.getUser.getName,
+            "id" -> x.getId
+          ))
+        }
         idx += 1
       }
     }
