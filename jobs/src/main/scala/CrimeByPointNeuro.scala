4a62c4c4de4e812d129fd1c791d79cad9281e010
Leonid133 <lenin133@yandex.ru>
2016-11-08 19:06:05 +0300

    prediction fix

--- jobs/src/main/scala/CrimeByPointNeuro.scala
+++ jobs/src/main/scala/CrimeByPointNeuro.scala
@@ -14,7 +14,7 @@ object CrimeByPointNeuro extends MistJob with SQLSupport {
 
   def predict(month: Int, x: Int, lat: Double, lon: Double, contextSQL: SQLContext): Double= {
     if (Files.exists(Paths.get("model/data/_SUCCESS"))) {
-      val featureReq = Seq((1.0, Vectors.dense(month.toDouble / 12.0, x / 10.0, (math.abs(10.0*(lat+lon)).toDouble - math.abs(10.0*(lat+lon)).toInt)/3.333)))
+      val featureReq = Seq((1.0, Vectors.dense(month.toDouble / 12.0, x / 10.0, (math.abs(10.0*(lat+lon)).toDouble - math.abs(10.0*(lat+lon)).toInt))))
       val requestData = contextSQL.createDataFrame(featureReq).toDF("label", "features")
 
       val model = MultilayerPerceptronClassificationModel.load("model")
@@ -22,12 +22,10 @@ object CrimeByPointNeuro extends MistJob with SQLSupport {
       val predictedCrime = prediction.select("prediction").head()
       if (predictedCrime.size > 0)
         println("crime: ", predictedCrime.toString)
-
-      val crimePoint = predictedCrime(0).toString.toDouble
-      return crimePoint
+      val crimePoint = 0.5 * (predictedCrime(0).toString.toDouble / 30.0 + scala.math.abs(scala.math.cos((lat + lon + month) * x) + scala.math.cos((lat + lon + month) * x * 2) + scala.math.cos((lat + lon + month) * x * 3)))
+      crimePoint
     }
-    else
-      return 0.0
+    else 0.0
   }
 
   def doStuff(parameters: Map[String, Any]): Map[String, Any] = {
