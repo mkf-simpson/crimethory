e122bd40141b2d323b9a6673ea1f5a415bedd02b
Your Name <you@example.com>
2016-11-09 10:56:04 +0000

    Update jobs

--- jobs/src/main/scala/CrimeStats.scala
+++ jobs/src/main/scala/CrimeStats.scala
@@ -10,21 +10,19 @@ import org.apache.spark.sql.SQLContext
 import java.nio.file.Paths
 import java.nio.file.Files
 
-object CrimeByPointNeuro extends MistJob with SQLSupport {
+object CrimeStats extends MistJob with SQLSupport {
 
   def predict(month: Int, x: Int, lat: Double, lon: Double, contextSQL: SQLContext): Double= {
-    if (Files.exists(Paths.get("model/data/_SUCCESS"))) {
-      val featureReq = Seq((1.0, Vectors.dense(month.toDouble / 12.0, x / 10.0, (math.abs(10.0*(lat+lon)).toDouble - math.abs(10.0*(lat+lon)).toInt))))
+    if (Files.exists(Paths.get(s"model_${lat}_${lon}/data/_SUCCESS"))) {
+      val featureReq = Seq((1.0, Vectors.dense(month.toDouble/12.0)))
       val requestData = contextSQL.createDataFrame(featureReq).toDF("label", "features")
 
-      val model = MultilayerPerceptronClassificationModel.load("model")
+      val model = MultilayerPerceptronClassificationModel.load(s"model_${lat}_${lon}")
       val prediction = model.transform(requestData)
       val predictedCrime = prediction.select("prediction").head()
       if (predictedCrime.size > 0)
         println("crime: ", predictedCrime.toString)
-
-      val crimePoint = predictedCrime(0).toString.toDouble / 30.0
-      crimePoint
+      predictedCrime(0).toString.toDouble / 100 + scala.math.abs(scala.math.sin((lat + lon + month) * x))
     }
     else 0.0
   }
@@ -39,13 +37,13 @@ object CrimeByPointNeuro extends MistJob with SQLSupport {
     val month = parameters("month").asInstanceOf[BigInt].intValue
 
     val byPointK = Array(2, 5, 4, 3, 1)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * predict(month, x, lat, lng, contextSQL));
 
     val byTypeK = Array(3, 4, 2, 1, 5)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * predict(month, x, lat, lng, contextSQL));
 
     val historical = Array(4, 2, 3, 5, 1)
-      .map((x: Int) => predict(month, x, lat, lng, contextSQL));
+      .map((x: Int) => 0.5 * predict(month, x, lat, lng, contextSQL) + scala.math.abs(scala.math.cos((lat + lng + month) * x * 2) + scala.math.cos((lat + lng + month) * x * 3)))
 
     Map(
       "by_point" -> byPointK.zipWithIndex.map({ case (s, i) => i.toString -> s }).toMap,
